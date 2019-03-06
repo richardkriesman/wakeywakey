77baed5edf407f6fbf3a388ffd709c6e05fd0847
ChaoticWeg <shawn@chaoticweg.cc>
2019-03-06 12:54:15 -0600

    HomeScreenClock: update time once per second

--- src/components/HomeScreen/HomeScreenClock.tsx
+++ src/components/HomeScreen/HomeScreenClock.tsx
@@ -1,10 +1,17 @@
 import React, { ReactNode } from "react";
-import { View, StyleSheet, ViewStyle } from "react-native";
+import { View, StyleSheet, ViewStyle, YellowBox } from "react-native";
 
 import { Text } from "react-native";
+import { number } from "prop-types";
 
-export interface HomeScreenClockProps {
-    wrapperStyle?   : ViewStyle;
+interface HomeScreenClockProps {
+    wrapperStyle? : ViewStyle;
+}
+
+interface HomeScreenClockState {
+    hours: string
+    minutes: string
+    am_pm: string
 }
 
 /**
@@ -12,35 +19,73 @@ export interface HomeScreenClockProps {
  * 
  * @author Shawn Lutch
  */
-export class HomeScreenClock extends React.Component<HomeScreenClockProps> {
+export class HomeScreenClock extends React.Component<HomeScreenClockProps, HomeScreenClockState> {
     constructor(props : HomeScreenClockProps) {
         super(props);
+
+        this.state = { hours: '', minutes: '', am_pm: '' };
     }
 
-    _pad(num : number) : string {
-        return (num < 10 ? '0' : '') + num;
+    /**
+     * Runs just before render().
+     * Update internal date initially, then schedule another update every second.
+     */
+    componentWillMount() : void {
+        this.updateInternalDate();
+        setInterval(this.updateInternalDate.bind(this), 1000);
+    }
+
+    /**
+     * Update the state using a `new Date()` as `now`.
+     */
+    updateInternalDate() : void {
+        let now : Date = new Date();
+
+        let hours : number = now.getHours();
+        hours = hours <= 12 ? hours : (hours % 12);
+
+        let isPm : boolean = now.getHours() >= 12;
+
+        this.setState({
+            hours: this._pad(hours, 1),
+            minutes: this._pad(now.getMinutes(), 2),
+            am_pm: isPm ? 'PM' : 'AM'
+        });
+    }
+
+    /**
+     * Pads a number out into a string with a minimum number of digits.
+     * e.g. `_pad(2, 2) => '02'`, `_pad(12, 1) => '12'`
+     * 
+     * @param num The number to pad
+     * @param digits The minimum number of digits to pad to
+     */
+    _pad(num : number, digits : number = 2) : string {
+        let result = '' + num;
+        while (result.length < digits) { result = '0' + result; }
+        return result;
     }
 
     render() : ReactNode {
-        let now = new Date();
-
         return (
             <View style={this.props.wrapperStyle}>
-                <Text style={styles.clock}>
-                    {/* {this._pad(now.getHours())}:{this._pad(now.getMinutes())} */}
-                    XX:XX
-                </Text>
+                <View style={styles.innerWrapper}>
+                    <Text style={styles.clockText}>
+                        {this.state.hours}:{this.state.minutes} {this.state.am_pm}
+                    </Text>
+                </View>
             </View>
         );
     }
 };
 
 const styles = StyleSheet.create({
-    clock: {
-        color: 'black',
-        fontSize: 70,
+    innerWrapper: {
         marginTop: 20,
         marginBottom: 20,
-        textAlign: 'center'
+    },
+    clockText: {
+        textAlign: 'center',
+        fontSize: 60,
     }
 });
