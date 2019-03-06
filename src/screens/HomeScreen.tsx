82b4fa08240a11a2967ce8b9152d33cdf9b0b368
ChaoticWeg <shawn@chaoticweg.cc>
2019-03-06 01:25:03 -0600

    bring styles back in

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,15 +1,16 @@
 import React, { ReactNode } from "react";
 import { View, StyleSheet } from "react-native";
 
-import Styles from "./styles/MainStyles";
 import SlideUpIndicator from "../components/HomeScreen/SlideUpIndicator";
 
 export default class HomeScreen extends React.Component {
 
     public render(): ReactNode {
         return (
-            <View style={Styles.container}>
-                <SlideUpIndicator />
+            <View style={ExtraStyles.container}>
+                <View style={ExtraStyles.bottom}>
+                    <SlideUpIndicator />
+                </View>
             </View>
         );
     }
@@ -19,5 +20,16 @@ export default class HomeScreen extends React.Component {
 // unused for now
 // tslint:disable-next-line:6133
 const ExtraStyles = StyleSheet.create({
-
+    container: {
+        marginTop: 20,
+        padding: 0,
+        flex: 1,
+        alignItems: 'center'
+    },
+    bottom: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'stretch',
+        marginBottom: -10
+    }
 });
