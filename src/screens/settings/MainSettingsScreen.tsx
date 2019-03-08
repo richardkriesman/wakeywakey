2b91e3ea62a9ed5760d94f67c8596cad783fa91d
ChaoticWeg <shawn@chaoticweg.cc>
2019-03-08 12:15:43 -0600

    settings: add docs to main screen

--- src/screens/settings/MainSettingsScreen.tsx
+++ src/screens/settings/MainSettingsScreen.tsx
@@ -3,10 +3,20 @@ import { View } from "react-native";
 
 import { NavigationScreenProp } from "react-navigation";
 
+/**
+ * Properties of the main settings screen.
+ * 
+ * @author Miika Raina
+ */
 export interface MainSettingsScreenProps {
     navigation: NavigationScreenProp<any, any>;
 }
 
+/**
+ * Main Settings screen.
+ * 
+ * @author Shawn Lutch, Miika Raina
+ */
 export default class MainSettingsScreen extends React.Component<MainSettingsScreenProps> {
     public constructor(props : MainSettingsScreenProps) {
         super(props);
