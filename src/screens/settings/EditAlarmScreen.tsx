ebc7c3771d9e31946d30097a69fb060be18ef7bb
ChaoticWeg <shawn@chaoticweg.cc>
2019-03-22 00:59:25 -0500

    navigation: extract navigationOptions to MainStackNavigator
    and stop using state to dictate header title because that won't work lol

--- src/screens/settings/EditAlarmScreen.tsx
+++ src/screens/settings/EditAlarmScreen.tsx
@@ -2,22 +2,15 @@ import React, { ReactNode } from "react";
 import { View } from "react-native";
 import { NavigationScreenProps } from "react-navigation";
 
+// tslint:disable-next-line:no-empty-interface
 export interface EditAlarmScreenState {
-    title: string;
+    // TODO add fields
 }
 
 export default class EditAlarmScreen extends React.Component<NavigationScreenProps, EditAlarmScreenState> {
 
-    private static defaultName: string = "Add Alarm";
-
     public constructor(props: NavigationScreenProps) {
         super(props);
-    }
-
-    public componentWillMount(): void {
-        this.setState({
-            title: this.props.navigation.getParam("title", EditAlarmScreen.defaultName)
-        });
     }
 
     public render(): ReactNode {
