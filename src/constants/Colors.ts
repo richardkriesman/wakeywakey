1ca47be1497182b3d93c11fad2ff3210d9ab3576
ChaoticWeg <shawn@chaoticweg.cc>
2019-03-22 01:31:12 -0500

    colors: add appleButtonRed and headerBackground
    also normalized colors to lower-case hex

--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -1,17 +1,21 @@
 const tintColor = "#2f95dc";
 
-const appleButtonBlue = "#007AFF";
+const appleButtonBlue = "#007aff";
+const appleButtonRed = "#ff3b30";
+const headerBackground = "#fff";
 
 export default {
     appleButtonBlue,
+    appleButtonRed,
     errorBackground: "red",
     errorText: "#fff",
+    headerBackground,
     noticeBackground: tintColor,
     noticeText: "#fff",
     tabBar: "#fefefe",
     tabIconDefault: "#ccc",
     tabIconSelected: tintColor,
     tintColor,
-    warningBackground: "#EAEB5E",
+    warningBackground: "#eaeB5e",
     warningText: "#666804"
 };
