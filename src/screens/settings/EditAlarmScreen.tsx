d1000a24422f631cb0ef8b0453cdc4fbaba21ceb
ChaoticWeg <shawn@chaoticweg.cc>
2019-03-22 02:18:58 -0500

    screens: add EditAlarmScreen header contents

--- src/screens/settings/EditAlarmScreen.tsx
+++ src/screens/settings/EditAlarmScreen.tsx
@@ -1,16 +1,43 @@
 import React, { ReactNode } from "react";
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { Button } from "react-native-elements";
 import { NavigationScreenProps } from "react-navigation";
+
+import Colors from "../../constants/Colors";
 
 // tslint:disable-next-line:no-empty-interface
 export interface EditAlarmScreenState {
-    // TODO add fields
+    headerTitle: string;
 }
 
 export default class EditAlarmScreen extends React.Component<NavigationScreenProps, EditAlarmScreenState> {
 
+    public static navigationOptions = ({ navigation }: NavigationScreenProps) => {
+        return {
+            headerLeft: (
+                <Button type="clear" titleStyle={styles.cancelButton} title="Cancel"
+                        onPress={() => {
+                            navigation.navigate("SettingsMain");
+                        }}/>
+            ),
+            headerRight: (
+                <Button type="clear" titleStyle={styles.saveButton} title="Save"
+                        onPress={() => {
+                            navigation.navigate("SettingsMain");
+                        }}/>
+            ),
+            title: navigation.getParam("title")
+        };
+    }
+
     public constructor(props: NavigationScreenProps) {
         super(props);
+    }
+
+    public componentWillMount(): void {
+        this.setState({
+            headerTitle: this.props.navigation.getParam("title")
+        });
     }
 
     public render(): ReactNode {
@@ -21,3 +48,15 @@ export default class EditAlarmScreen extends React.Component<NavigationScreenPro
         );
     }
 }
+
+const styles = StyleSheet.create({
+    cancelButton: {
+        color: Colors.appleButtonRed,
+        marginLeft: 10
+    },
+    saveButton: {
+        color: Colors.appleButtonBlue,
+        fontWeight: "500",
+        marginRight: 10
+    }
+});
