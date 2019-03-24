bafcd68f30df4f3a9c8273fa42f9a513cf81871a
ChaoticWeg <shawn@chaoticweg.cc>
2019-03-23 21:22:49 -0500

    models: add key to alarm model

--- src/models/AlarmModel.ts
+++ src/models/AlarmModel.ts
@@ -1,3 +1,4 @@
+export const DayNames: string[] = [ "Su", "M", "T", "W", "Th", "Fr", "Sa" ];
 export enum DayOfWeek { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday }
 
 export interface AlarmModel {
@@ -5,18 +6,21 @@ export interface AlarmModel {
     sleepTime: Date;
     wakeUpTime: Date;
     getUpTime: Date;
+    key: number;
 }
 
 export const TestAlarms: AlarmModel[] = [
     {
         days: [ DayOfWeek.Monday, DayOfWeek.Tuesday, DayOfWeek.Wednesday, DayOfWeek.Thursday, DayOfWeek.Friday ],
         getUpTime: new Date(),
+        key: 0,
         sleepTime: new Date(),
         wakeUpTime: new Date()
     },
     {
         days: [ DayOfWeek.Sunday, DayOfWeek.Saturday ],
         getUpTime: new Date(),
+        key: 1,
         sleepTime: new Date(),
         wakeUpTime: new Date()
     }
