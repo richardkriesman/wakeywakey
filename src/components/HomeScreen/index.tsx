afd4eb1d230a4722b8f408ef695370d9c3e485a2
ChaoticWeg <shawn@chaoticweg.cc>
2019-03-27 11:18:44 -0500

    coverage: ignore helper exports

--- src/components/HomeScreen/index.tsx
+++ src/components/HomeScreen/index.tsx
@@ -1,3 +1,4 @@
+/* istanbul ignore file */
 export { HomeScreenClock } from "./HomeScreenClock";
 export { SlideUpIndicator } from "./SlideUpIndicator";
 export { SnoozeButton } from "./SnoozeButton";
