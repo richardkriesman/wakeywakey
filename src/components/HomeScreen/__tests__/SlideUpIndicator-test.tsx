3d5460dd4be2af7beec17ae42407576e44101c3c
ChaoticWeg <shawn@chaoticweg.cc>
2019-03-27 01:01:22 -0500

    tests: add SlideUpIndicator tests

---
+++ src/components/HomeScreen/__tests__/SlideUpIndicator-test.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { Button } from "react-native-elements";
+import renderer from "react-test-renderer";
+import { SlideUpIndicator } from "../SlideUpIndicator";
+
+describe("SlideUpIndicator", () => {
+
+    it("renders properly", () => {
+        const tree = renderer.create(<SlideUpIndicator onPress={() => void 0}/>);
+        expect(tree).toMatchSnapshot();
+    });
+
+    it("reacts to onPress", () => {
+        const resultText: string = "'Slide Up' indicator pressed";
+
+        const onPress = jest.fn();
+        onPress.mockReturnValue(resultText);
+
+        const tree = renderer.create(<SlideUpIndicator onPress={onPress}/>);
+        expect(tree.root.findByType(Button).props.onPress()).toBe(resultText);
+    });
+
+});
