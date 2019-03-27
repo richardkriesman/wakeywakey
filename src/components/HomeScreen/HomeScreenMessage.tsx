34b0c952449ca8309af4f029937ba63d93188aa7
ChaoticWeg <shawn@chaoticweg.cc>
2019-03-27 00:23:57 -0500

    tests: make HomeScreenMessage more conducive to testing

--- src/components/HomeScreen/HomeScreenMessage.tsx
+++ src/components/HomeScreen/HomeScreenMessage.tsx
@@ -2,17 +2,33 @@ import React, { ReactNode } from "react";
 import { StyleSheet, Text } from "react-native";
 
 export interface HomeScreenMessageProps {
+    initialText: string;
+}
+
+export interface HomeScreenMessageState {
     text: string;
 }
 
-export class HomeScreenMessage extends React.Component<HomeScreenMessageProps> {
+export class HomeScreenMessage extends React.Component<HomeScreenMessageProps, HomeScreenMessageState> {
     public constructor(props: HomeScreenMessageProps) {
         super(props);
     }
 
+    public componentWillMount(): void {
+        this.setState({ text: this.props.initialText });
+    }
+
+    public get text(): string {
+        return this.state.text;
+    }
+
+    public set text(text: string) {
+        this.setState({ text });
+    }
+
     public render(): ReactNode {
         return (
-            <Text style={styles.message}>{this.props.text}</Text>
+            <Text style={styles.message}>{this.state.text}</Text>
         );
     }
 }
