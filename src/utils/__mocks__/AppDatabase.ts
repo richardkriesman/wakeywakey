22f5369a671f4af88547cc2d9cfe236f384689bd
Richard Kriesman <rkriesman@gmail.com>
2019-04-15 10:22:50 -0500

    Fixed some (but not all, yet) tests and updated the AppDatabase mock.

--- src/utils/__mocks__/AppDatabase.ts
+++ src/utils/__mocks__/AppDatabase.ts
@@ -1,3 +1,7 @@
+import {Service} from "../Service";
+import {EmitterSet} from "../watcher";
+import {Model} from "../Model";
+
 export class AppDatabase {
 
     public static get initCount(): number {
@@ -13,10 +17,22 @@ export class AppDatabase {
      */
     public mockPreferences: { [key: string]: string } = {};
 
+    private services: Map<string, Service> = new Map();
+    private emitters: Map<string, EmitterSet<any>> = new Map();
+
     private constructor() {}
 
     public async doesTableExist(name: string): Promise<boolean> {
         throw new Error("Implement this mock function");
+    }
+
+    public getEmitterSet<T extends Model>(name: string): EmitterSet<T> {
+        let emitters: EmitterSet<T>|undefined = this.emitters.get(name);
+        if (!emitters) {
+            emitters = new EmitterSet<T>();
+            this.emitters.set(name, emitters);
+        }
+        return emitters;
     }
 
     public async getPreference(key: string): Promise<string|null> {
@@ -25,6 +41,13 @@ export class AppDatabase {
         } else {
             return null;
         }
+    }
+
+    public getService<T extends Service>(service: new(db: AppDatabase) => T): T {
+        if (!this.services.has(service.name)) {
+            this.services.set(service.name, new service(this));
+        }
+        return this.services.get(service.name) as any;
     }
 
     public execute(sql: DOMString, args: ObjectArray = []): Promise<SQLResultSet> {
