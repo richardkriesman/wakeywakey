a7086343fca7805acabee96841438b22c302ad6c
Cody Kyrk <cody.kyrk@gmail.com>
2019-04-19 09:46:16 -0500

    Created vertical slider component

--- src/components/HomeScreen/index.tsx
+++ src/components/HomeScreen/index.tsx
@@ -5,3 +5,4 @@
 export { HomeScreenClock as Clock } from "./HomeScreenClock";
 export { SlideUpIndicator } from "./SlideUpIndicator";
 export { SnoozeButton } from "./SnoozeButton";
+export { LockScreenSlider } from "./LockScreenSlider";
