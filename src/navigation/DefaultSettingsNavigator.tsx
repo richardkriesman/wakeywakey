b0b2659a7b2ed19e72f86c11f93db6b36bd69e8c
ChaoticWeg <shawn@chaoticweg.cc>
2019-04-23 13:17:16 -0500

    remove unnecessary copy/pasted nonsense
    im good! at this!!

--- src/navigation/DefaultSettingsNavigator.tsx
+++ src/navigation/DefaultSettingsNavigator.tsx
@@ -1,35 +1,11 @@
 import React from "react";
-import { BottomTabNavigatorConfig, createBottomTabNavigator, NavigationScreenProps } from "react-navigation";
-import { HeaderIconButton } from "../components/HeaderIconButton";
-import { Schedule } from "../models/Schedule";
+import { BottomTabNavigatorConfig, createBottomTabNavigator } from "react-navigation";
 import { AppSettingsScreen } from "../screens/settings/AppSettingsScreen";
 
 import SchedulesListScreen from "../screens/settings/SchedulesListScreen";
-import { UIScreen } from "../utils/screen/UIScreen";
 
 const DefaultSettingsScreens = {
     SchedulesList: {
-        navigationOptions: ({ navigation }: NavigationScreenProps) => ({
-            headerRight: <HeaderIconButton
-                icon="add"
-                onPress={() => {
-
-                    /*
-                     * I'm going to be honest I copied and pasted and tweaked this from Richard's example on the
-                     * MainStackNavigator. -sL
-                     */
-                    const screen: UIScreen = navigation.getParam("screen");
-                    const schedule: Schedule = navigation.getParam("schedule");
-
-                    // present the edit alarm screen
-                    screen.present("EditSchedule", {
-                        schedule,
-                        title: "Add Schedule"
-                    });
-
-                }} />,
-            title: navigation.getParam("title", "Schedules")
-        }),
         screen: SchedulesListScreen
     },
 
