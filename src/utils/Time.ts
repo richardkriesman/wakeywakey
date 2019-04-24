aec51e6eb3bcf4cbc1b011db7f497cc6134b0163
Richard Kriesman <rkriesman@gmail.com>
2019-04-23 21:57:06 -0500

    Added functions for adding and subtracting Times.

--- src/utils/Time.ts
+++ src/utils/Time.ts
@@ -9,6 +9,10 @@ export class Time {
      * @param totalSeconds
      */
     public static createFromTotalSeconds(totalSeconds: number): Time {
+        if (totalSeconds < 0) {
+            throw new Error("Time values cannot be negative");
+        }
+
         const time = new Time();
         time._hour = Math.floor(totalSeconds / 3600);
         time._minute = Math.floor((totalSeconds - (time.hour * 3600)) / 60);
@@ -17,6 +21,10 @@ export class Time {
     }
 
     public static createFromDisplayTime(hour: number, minute: number, second?: number): Time {
+        if (hour < 0 || minute < 0 || second < 0) {
+            throw new Error("Time values cannot be negative");
+        }
+
         const time = new Time();
         time._hour = hour;
         time._minute = minute;
@@ -67,11 +75,58 @@ export class Time {
         return (this.hour * 3600) + (this.minute * 60) + this.second;
     }
 
+    /**
+     * Returns a new Time with the number of hours, minutes, and seconds added to this Time. The new Time will properly
+     * wrap around to the next day if the new Time exceeds 24 hours.
+     *
+     * @param hour
+     * @param minute
+     * @param second
+     */
+    public add(hour: number, minute: number = 0, second: number = 0): Time {
+        const diffTime: Time = Time.createFromDisplayTime(hour, minute, second); // time delta
+
+        // wrap around to next day if time exceeds 24 hours
+        let totalSeconds: number = this.totalSeconds + diffTime.totalSeconds;
+        while (totalSeconds >= 86400) { // 24 hours in seconds
+            totalSeconds -= 86400;
+        }
+
+        return Time.createFromTotalSeconds(totalSeconds);
+    }
+
     public equals(time: Time): boolean {
         return this.totalSeconds === time.totalSeconds;
     }
 
+    public isFutureOrEqual(rel: Time): boolean {
+        return this.totalSeconds <= rel.totalSeconds;
+    }
+
     public isPastOrEqual(rel: Time): boolean {
         return this.totalSeconds >= rel.totalSeconds;
+    }
+
+    /**
+     * Returns a new Time with the number of hours, minutes, and seconds subtracted from this Time. The new Time will
+     * properly wrap around to the previous day if the new Time is less than 0 total seconds.
+     *
+     * A dom() function will be added later which will return a Time that is compatible with sub(). Okay, that was a
+     * bad joke but I'm not sorry.
+     *
+     * @param hour
+     * @param minute
+     * @param second
+     */
+    public sub(hour: number, minute: number = 0, second: number = 0): Time {
+        const diffTime: Time = Time.createFromDisplayTime(hour, minute, second); // time delta
+
+        // wrap around to previous day if time is less than 0
+        let totalSeconds: number = this.totalSeconds - diffTime.totalSeconds;
+        while (totalSeconds < 0) {
+            totalSeconds += 86400;
+        }
+
+        return Time.createFromTotalSeconds(totalSeconds);
     }
 }
