1409ad21842b1463224622a5be42c08f4c567057
Richard Kriesman <rkriesman@gmail.com>
2019-04-24 18:30:54 -0500

    Fixed tests after changing how params work in TestEnvironment.

--- src/screens/settings/__tests__/ScheduleOptionsScreen-test.tsx
+++ src/screens/settings/__tests__/ScheduleOptionsScreen-test.tsx
@@ -1,18 +1,36 @@
 import * as React from "react";
 import renderer from "react-test-renderer";
+import { Schedule } from "../../../models";
+import { ScheduleService } from "../../../services";
 import { TestEnvironment } from "../../../utils/testing";
 import { ScheduleOptionsScreen } from "../ScheduleOptionsScreen";
 
+jest.mock("../../../services/ScheduleService");
+
 let env: TestEnvironment;
+let schedule: Schedule;
 beforeEach((done) => {
     TestEnvironment.init()
         .then((newEnv) => {
             env = newEnv;
+
+            // create a new schedule
+            return env.db.getService(ScheduleService).create("Test schedule");
+        })
+        .then((newSchedule) => {
+            schedule = newSchedule;
             done();
         });
 });
 
 it("renders correctly", () => {
-    const tree = renderer.create(<ScheduleOptionsScreen {...env.emptyUIScreenProps}/>);
+
+    // build props
+    const props = env.emptyUIScreenProps;
+    props.navigation.setParams({ schedule });
+
+    // render the tree
+    const tree = renderer.create(<ScheduleOptionsScreen {...props}/>);
     expect(tree.toJSON()).toMatchSnapshot();
+
 });
