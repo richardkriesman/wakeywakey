3ed919dc1c6117b4755e072ac4c2fb7c73ea69ed
ChaoticWeg <shawn@chaoticweg.cc>
2019-04-25 23:59:26 -0500

    please dont ever do this please GOD dont ever dO THIS.

--- src/screens/PasscodeGateScreen.tsx
+++ src/screens/PasscodeGateScreen.tsx
@@ -32,7 +32,7 @@ export interface PasscodeGateScreenState {
 */
 
 @NoHeader
-@Title("Home")
+@Title("Home") // DON'T DO THIS NEVER DO THIS
 export default class PasscodeGateScreen extends UIScreen<{}, PasscodeGateScreenState> {
     public constructor(props: NavigationScreenProps) {
         super(props);
