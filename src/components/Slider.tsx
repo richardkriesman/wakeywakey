d05d426b7087f0d43857d7fc29efba4963c11bf3
Richard Kriesman <rkriesman@gmail.com>
2019-05-04 14:46:01 -0500

    Added tap to expand/contract to slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -7,11 +7,18 @@ import { Animated, GestureResponderEvent, LayoutChangeEvent, StyleSheet, View, V
 
 import { Button, Icon } from "react-native-elements";
 import Colors from "../constants/Colors";
+import Timeout = NodeJS.Timeout;
 
-enum MotionState {
-    IDLE = 0,
-    DRAGGING = 1,
-    ANIMATING = 2
+enum SliderAnimationDirection {
+    Bottom = 0,
+    Top = 1
+}
+
+enum SliderMotionState {
+    Idle = 0,
+    Dragging = 1,
+    Press = 3,
+    Animating = 2
 }
 
 interface PasscodeGateSliderState {
@@ -29,7 +36,9 @@ export interface PasscodeGateSliderProps {
 export class Slider extends React.Component<PasscodeGateSliderProps, PasscodeGateSliderState> {
 
     private contentHeight: number = 0; // computed height of the content
-    private motionState: MotionState = MotionState.IDLE; // whether the slider is animated, being dragged, or idle
+    private motionState: SliderMotionState = SliderMotionState.Idle; // whether the slider is moving or idle
+    private pressTimeout?: Timeout;
+    private yDragStart: number = 0; // y value when the drag was started
     private yOffset: number = 0; // offset from the point of the user's touch - prevents jumping during responder grant
 
     public constructor(props: PasscodeGateSliderProps) {
@@ -41,7 +50,7 @@ export class Slider extends React.Component<PasscodeGateSliderProps, PasscodeGat
 
         // create dynamic style for parent view, allowing us to set the x and y coords
         const posStyle = {
-            top: this.motionState === MotionState.ANIMATING ? this.state.yAnim : this.yDrag
+            top: this.motionState === SliderMotionState.Animating ? this.state.yAnim : this.yDrag
         };
 
         return (
@@ -74,21 +83,80 @@ export class Slider extends React.Component<PasscodeGateSliderProps, PasscodeGat
         );
     }
 
+    private animate(direction: SliderAnimationDirection): Promise<void> {
+        return new Promise<void>((resolve) => {
+
+            // update motion state
+            this.motionState = SliderMotionState.Animating;
+
+            // update yAnim
+            this.setState({
+                yAnim: new Animated.Value(this.yDrag)
+            }, () => { // yAnim has been updated
+
+                // determine which bound to animate to
+                let toValue: number;
+                if (direction === SliderAnimationDirection.Top) {
+                    toValue = (this.props.initialTop - this.contentHeight);
+                } else { // animate to bottom
+                    toValue = this.props.initialTop;
+                }
+
+                // start the animation
+                Animated.timing(this.state.yAnim, {
+                    duration: 250, // TODO: use a timing curve from material design for this, they look way better
+                    toValue
+                }).start(() => {
+                    this.setState({ // update yDrag so dragging starts from the correct position
+                        yDrag: toValue
+                    }, () => { // yDrag has been updated, set motion state to idle so we can drag again
+                        this.motionState = SliderMotionState.Idle;
+                        resolve();
+                    });
+                });
+
+            });
+        });
+    }
+
+    private onPress(): void {
+        const distanceToTop = this.yDrag - (this.props.initialTop - this.contentHeight);
+        const distanceToBottom = this.props.initialTop - this.yDrag;
+        if (distanceToTop >= distanceToBottom) { // move in the direction opposite to where we currently are
+            this.animate(SliderAnimationDirection.Top);
+        } else {
+            this.animate(SliderAnimationDirection.Bottom);
+        }
+    }
+
     private onContentLayout(event: LayoutChangeEvent): void {
         this.contentHeight = event.nativeEvent.layout.height;
     }
 
     private onResponderGrant(event: GestureResponderEvent): void {
-        if (this.motionState !== MotionState.IDLE) { // only allow dragging if slider is idle
+        if (this.motionState !== SliderMotionState.Idle) { // only allow dragging if slider is idle
             return;
         }
 
-        this.motionState = MotionState.DRAGGING;
+        // set y offset for drag
         this.yOffset = event.nativeEvent.pageY - this.yDrag;
+        this.yDragStart = event.nativeEvent.pageY;
+
+        // by default, we're going to consider this a press - if it's dragged too far, we'll consider it a drag
+        this.motionState = SliderMotionState.Press;
+
     }
 
     private onResponderMove(event: GestureResponderEvent): void {
-        if (this.motionState !== MotionState.DRAGGING) { // only move the slider if it's draggable
+
+        // if the slider was dragged too far, we should consider it a drag
+        if (this.motionState === SliderMotionState.Press
+                && Math.abs(event.nativeEvent.pageY - this.yDragStart) > 20) {
+            this.motionState = SliderMotionState.Dragging;
+        }
+
+        // only move the slider if it's draggable
+        if (this.motionState !== SliderMotionState.Dragging) {
             return;
         }
 
@@ -108,42 +176,24 @@ export class Slider extends React.Component<PasscodeGateSliderProps, PasscodeGat
     }
 
     private onResponderRelease(event: GestureResponderEvent): void {
+        if (this.motionState === SliderMotionState.Press) { // press, fire the press handler
+            this.motionState = SliderMotionState.Idle;
+            this.onPress();
+            return;
+        }
+
         if (this.yDrag >= this.props.initialTop || this.yDrag <= this.props.initialTop - this.contentHeight) { // bounds
-            this.motionState = MotionState.IDLE;
+            this.motionState = SliderMotionState.Idle;
         } else { // slider is not at bounds, animate to bounds
             const distanceToTop = this.yDrag - (this.props.initialTop - this.contentHeight);
             const distanceToBottom = this.props.initialTop - this.yDrag;
 
-            // update motion state
-            this.motionState = MotionState.ANIMATING;
-
-            // update yAnim
-            this.setState({
-                yAnim: new Animated.Value(this.yDrag)
-            }, () => { // yAnim has been updated
-
-                // determine which bound to animate to
-                // TODO: adjust this to bias in the opposite direction?
-                let toValue: number;
-                if (distanceToTop <= distanceToBottom) { // animate to top
-                    toValue = (this.props.initialTop - this.contentHeight);
-                } else { // animate to bottom
-                    toValue = this.props.initialTop;
-                }
-
-                // start the animation
-                Animated.timing(this.state.yAnim, {
-                    duration: 250, // TODO: use a timing curve from material design for this, they look way better
-                    toValue
-                }).start(() => {
-                    this.setState({ // update yDrag so dragging starts from the correct position
-                        yDrag: toValue
-                    }, () => { // yDrag has been updated, set motion state to idle so we can drag again
-                        this.motionState = MotionState.IDLE;
-                    });
-                });
-
-            });
+            // TODO: adjust this to bias in the opposite direction?
+            if (distanceToTop <= distanceToBottom) {
+                this.animate(SliderAnimationDirection.Top);
+            } else {
+                this.animate(SliderAnimationDirection.Bottom);
+            }
         }
     }
 
